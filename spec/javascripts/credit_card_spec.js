16a2cf8c6b713eb1301ffe1084286cf0d6405c71
Sam Joseph <tansaku@gmail.com>
2013-12-20 13:17:11 +0000

    jasmine-jquery added and new test on fixture

--- spec/javascripts/credit_card_spec.js
+++ spec/javascripts/credit_card_spec.js
@@ -7,5 +7,13 @@ describe("CreditCard", function() {
         expect(CreditCard.validNumber ("4111 1111 1111-1111")).toBeTruthy();
         expect(CreditCard.validNumber ("4111 1111 1211-1111")).toBeFalsy();
     });
+
+    it("validates when text field loses focus", function() {
+        loadFixtures("order_form.html");
+        $("#card_number").validateCreditCardNumber();
+        $("#card_number").val("123");
+        $("#card_number").blur();
+        expect($("#card_number_error")).toHaveText("Invalid credit card number.");
+    });
 });
 
