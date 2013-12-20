aac530cff2dbafb94583759071b2a612e46132d7
Sam Joseph <tansaku@gmail.com>
2013-12-20 12:41:16 +0000

    vanilla rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JasmineRailscast::Application.config.secret_key_base = '9a5ee39e02583225fc8c7a1035d496ea335270c1928ed6d9b944406dbcad8361b4f7bbe1932f6d5f40a5352f66ca0014024cc74598d4657fa2d26aab7d58ef94'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+JasmineRailscast::Application.config.session_store :cookie_store, key: '_JasmineRailscast_session'
